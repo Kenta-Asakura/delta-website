a4bd1c7e6ec1779e3583f642d948b6faeda7c624
kenta asakura <asakura.kenta963420@gmail.com>
2023-08-31 12:12:40 +0900

    Nav and Logo within Header created, also Button.styled.js created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import { StyledHeader } from './styles/Header.styled'
+import { StyledHeader, Nav, Logo } from './styles/Header.styled'
+import { Container } from './styles/Container.styled'
+import { Button } from './styles/Button.styled'
 
 export default function Header() {
   return (
     <StyledHeader bg='red'>
-      <h1>Hubble</h1>
+      <Container>
+        <Nav>
+          <Logo src='./images/logo.svg'></Logo>
+          <Button>Try It Free</Button>
+        </Nav>
+        <h1>Hubble</h1>
+      </Container>
     </StyledHeader>
   )
 }
