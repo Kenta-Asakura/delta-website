6af8f4097adecb0b7c2bc0f6c456ec2878357ae2
kenta asakura <asakura.kenta963420@gmail.com>
2023-09-08 21:24:13 +0900

    Card.styled.js created and implemented

---
+++ src/components/styles/Card.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+
+export const StyledCard = styled.div`
+  display: flex;
+  align-items: centerl;
+  background-color: #fff;
+  border-radius: 15px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+  margin: 40px;
+  padding: 60px;
+`
